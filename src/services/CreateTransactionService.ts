03db153b21bc4bcbdb233070ff69f7cac0d3e2f6
lpbborges <luizpaulobborges@gmail.com>
2020-04-20 23:21:27 -0300

    Entrega desafio

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,54 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+
+import CreateCategoryService from './CreateCategoryService';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const createCategoryService = new CreateCategoryService();
+
+    if (type === 'outcome') {
+      const { total } = await transactionsRepository.getBalance();
+
+      if (total - value < 0) {
+        throw new AppError(
+          'Your outcomes should not overpass your incomes',
+          400,
+        );
+      }
+    }
+
+    const { id: category_id } = await createCategoryService.execute({
+      title: category,
+    });
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
